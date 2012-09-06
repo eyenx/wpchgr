ee08114ea342ae9992758072b49b8a0703c84005
gnomeye <gnomeye@gmail.com>
2012-09-07 00:27:35 +0200

    wpchgr.py with regexp

--- wpchgr.py
+++ wpchgr.py
@@ -2,7 +2,7 @@
 
 ## this script downloads a random wallpaper from http://wallbase.net. Standard Resolution = 1920 x 1080 FullHD
 
-import os,sys,time,subprocess,urllib.request
+import os,sys,time,subprocess,urllib.request,re
 
 # variables
 
@@ -16,27 +16,44 @@ archive='/media/exthome/img/wallpapers/wallbase/'
 
 def getimghtml():
 	html = urllib.request.urlopen(site)
-	text = html.read()
-	text = str(text)
-	pos = text.index(finder)
-	wallurl = text[pos:pos+op]
-	imghtml = urllib.request.urlopen(wallurl)
-	imgtext = imghtml.read()
-	imgtext = str(imgtext)
-	return(imgtext)
+	pat=re.compile(".*\"(http:\/\/wallbase.cc\/wallpaper\/[0-9]+)\"")
+	for line in html.readlines():
+		line=str(line)
+		if pat.match(line):
+			mtch=pat.search(line)
+			res=mtch.group(1)
+			break
+	imghtml=urllib.request.urlopen(res)
+	pat=re.compile(".*img src\=\"(http.*wallpaper\-[0-9]+\.jpg)\".*")
+	for line in imghtml.readlines():
+		line=str(line)
+		if pat.match(line):
+			mtch=pat.search(line)
+			res=mtch.group(1)
+			break
+	return(res)
+
+
+
 
 def getit(url):
-	subprocess.call(['wget',url,'-O',archive+'wallbase-'+url[-10:]],shell=False)
-	subprocess.call(['cp',archive+'wallbase-'+url[-10:],defdir],shell=False)
+	img=urllib.request.urlopen(url)
+	arch=open(archive+'wallbase-'+url[-10:],'wb')
+	arch.write(img.read())
+	arch.close()
+	arch=open(archive+'wallbase-'+url[-10:],'rb')
+	wall=open(defdir,'wb')
+	wall.write(arch.read())
+	wall.close()
+	arch.close()
 	subprocess.Popen('DISPLAY=:0.0 feh --bg-scale '+defdir,shell=True)
+
+	
 
 def testimghtml():
 	text = getimghtml()
 	while '.jpg' not in text:
 		text=getimghtml()
-	point=text.index('.jpg')
-	stop=text[point-100:point+4].index('http://')
-	url=(text[point-100:point+4][stop:])
-	getit(url)
+	getit(text)
 
 testimghtml()
