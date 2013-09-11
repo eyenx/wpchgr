03c078f3222c365b12976a92d57c8b0843362033
gnomeye <gnomeye@gmail.com>
2013-09-11 23:01:05 +0200

    wallbase.cc changed back to non b64url

--- wpchgr.pl
+++ wpchgr.pl
@@ -7,7 +7,8 @@ use LWP::Simple;
 use File::Copy;
 use MIME::Base64;
 
-my $URL='http://wallbase.cc/random/all/eqeq/1920x1080/0/100/32';
+#my $URL='http://wallbase.cc/random/all/eqeq/1920x1080/0/100/32';
+my $URL="http://wallbase.cc/random?section=wallpapers&q=&res_opt=eqeq&res=1920x1080&thpp=32&purity=100&board=21&aspect=0.00";
 my $finder='http://wallbase.cc/wallpaper/';
 my $filepath='/tmp/wall.jpg';
 my $archive='/media/exthome/img/wallpapers/wallbase/';
@@ -23,14 +24,22 @@ sub b64url{
 	$wallurl =~ m/.*src\=.*\+B\(\'(.*)\'\)\+.*/;
 	return decode_base64($1);
 }
+# w/o decoding
+sub jpgurl{
+  my $wallurl=randomwall();
+  $wallurl =~ m/.*src\=\"(http:\/\/wallpapers.wallbase.cc\/.*)\" class/;
+  return $1;
+}
 # get decoded url, test if jpg and return it - if not jpg redo the process (max of 3 times)
 sub wallurl{
-	my $wurl=b64url();
+  #my $wurl=b64url();
+  my $wurl=jpgurl();
 	my $count=1;
 	until ($wurl =~ /http\:.*wallpaper\-\d*.jpg/ or $count > 3){
 		sleep 3;
 		++$count;
-		my $wurl=b64url();}
+#		my $wurl=b64url();}
+		my $wurl=jpgurl();}
 	return $wurl;
 }
 
